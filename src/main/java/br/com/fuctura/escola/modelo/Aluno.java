c4de85453073be5e1b5011b09a1c0b0f3769aef1
Bergson Brito Barros <bergson.barros@serpro.gov.br>
2022-07-27 18:43:16 -0300

    Adicao inicial do projeto

---
+++ src/main/java/br/com/fuctura/escola/modelo/Aluno.java
@@ -0,0 +1,16 @@
+package br.com.fuctura.escola.modelo;
+
+import java.time.LocalDateTime;
+
+public class Aluno {
+
+	private Long id;
+	private String cpf;
+	private String nome;
+	private String email;
+	private String fone;
+	private LocalDateTime dataNascimento;
+	// CONVENCIONAL, MONITOR
+	private String tipo;
+	
+}
